2b9a8ea71763b33bd64bf70845ff3890c869530c
chzgustavo <g.choquevilca@gmail.com>
2021-03-07 20:30:26 -0300

    update image

--- source/main_test.go
+++ source/main_test.go
@@ -21,7 +21,7 @@ func Test_handler(t *testing.T) {
 	}{
 		{
 			"status 1",
-			args{"/", http.StatusOK, fmt.Sprintf("Hola Mund0!\nVersion: 1.0.0\nHello from %s\n", host)},
+			args{"/", http.StatusOK, fmt.Sprintf("Hola Mundo!\nVersion: 1.0.0\nHello from %s\n", host)},
 		},
 	}
 
