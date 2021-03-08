2aca657fb1b9e51321b8bf512f0e4d919edcf491
chzgustavo <g.choquevilca@gmail.com>
2021-03-08 17:08:32 -0300

    commit 10

--- source/main.go
+++ source/main.go
@@ -32,7 +32,6 @@ func handler(w http.ResponseWriter, r *http.Request){
 	if err != nil {
 		log.Panicf("not able to write http output: %s", err)
 	}
-
 	/*resp = fmt.Sprintf("\nVersion: 1.0.0")
 	_, err = w.Write([]byte(resp))
 	if err != nil {
